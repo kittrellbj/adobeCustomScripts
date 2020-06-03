96fff4c2b781b9f4f837a8b6b5a0f330304196c7
Brian Kittrell <kittrellbj@gmail.com>
2020-06-02 20:40:38 -0500

    Add files via upload

---
+++ filenameFromFieldjs/filenameFromField.js
@@ -0,0 +1,19 @@
+// filenameFromField.js
+// Author: Brian Kittrell
+// Home:   
+// Description: This javascript file goes in your Adobe Javascript directory, such as C:\Program Files\Adobe\Acrobat\Javascript.
+
+var saveFromField = app.trustedFunction(
+    function(documentObject, filePath, fileName) {
+        filePath = filePath.replace(/([^/])$/, "$1/");
+        // Attempt to save the file based on the callback parameters
+        try {
+            app.beginPriv();
+            documentObject.saveAs(filePath + fileName);
+            app.endPriv();
+        // If there was an error, output the error information for the callback
+        } catch (errorMsg) {
+        app.alert("There were errors while attempting to save the file: " + errorMsg);
+        }
+    }
+  );
---
+++ filenameFromFieldjs/filenameFromField_adobe.js
@@ -0,0 +1,17 @@
+// filenameFromField_adobe.js
+// Author: Brian Kittrell
+// Home:   
+// Description: This code snippet goes inside Adobe PDF attached to the save button in the form.
+//              To use, create a button on the PDF form (or use this to create other fucntionality) so that when
+//              this script is called, it is executed from the form field you are using to call the method.
+
+var directory = "/the/full/path/"; // Change this. This is the full path where you want the document to be saved once the button is clicked.
+if (typeof(saveFromField) == "function") {
+    saveFromField(this, directory, this.getField("NamingField").value + ".pdf"); // Change NamingField to the field governing the save name of the final document.
+    // If you want more complexity to the final document name, such as adding the current date, you could use something like:
+    // var currentDate = new Date();
+    // var formattedDate = currentDate.getFullYear() + "-" + (currentDate.getMonth() + 1) + "-" + currentDate.getDate();  // Formats: 2020-1-1
+    // this.getfield("NamingField").value + "_" + formattedDate + ".pdf"; // Results in filename: NamingField_2020-1-1.pdf
+} else {
+    app.alert("Unable to save."); // Catch errors.
+}
